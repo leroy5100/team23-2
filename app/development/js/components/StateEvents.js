af8ba9da6516165652dd793a3ce20fc22e4def0b
leroy5100 <leroyvanspronsen@hotmail.com>
2017-09-27 15:18:21 +0200

    styling partial and layout

--- app/development/js/components/StateEvents.js
+++ app/development/js/components/StateEvents.js
@@ -7,7 +7,9 @@ export default class StateEvents {
 
     eventHandlers(){
 		$('body')
-		.on('click', '.nav__menu_btn', this.displayMenu.bind(this))
+		.on('click', '.nav__menu_btn', this.displayMenu.bind(this));
+		$(window).scroll(window, this.fixHeader);
+
     }
 
     displayMenu(event){
@@ -17,4 +19,22 @@ export default class StateEvents {
 		$('.nav__menu .nav__icon').toggleClass('active');
 		$('.nav__mobile__bg').toggleClass('active');
     }
+	fixHeader(){
+	    let header = $('.page-head');
+	    let scrollHeight = $(window).scrollTop();
+	    $('body').addClass('fixed-header');
+	    let marginTop = header.outerHeight();
+	  	let offsetHeight = 100;
+	  	if ($(window).width() <= 768) {
+	  		offsetHeight = 100;
+		}
+	    if (scrollHeight > 1) {
+			header.addClass('is-fixed');
+			$('.main').css('margin-top', 200);
+	    }
+		else {
+	      	header.removeClass('is-fixed');
+	  		$('.main').css('margin-top', offsetHeight);
+	    }
+  }
 }
--- index.php
+++ index.php
@@ -11,31 +11,94 @@
 	<!--[if lt IE 9]>
 	<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
 	<![endif]-->
-	<header class="page-head">
+	<header class="page-head flex" style="background-image: url('./public/img/background-wood.jpg');">
 		<div class="wrapper">
-			<h1>Bartender ding des doods</h1>
-			<h4>Presented by #Team23</h4>
 			<nav class="nav-container">
 				<ul class="nav-list flex flex-justify-around flex-wrap">
-					<li class="nav-list-item"><a href="#page-introduction">Introduction</a></li>
-					<li class="nav-list-item"><a href="#page-highlight">Ons product</a></li>
-					<li class="nav-list-item"><a href="#page-introduction">Introduction</a></li>
-					<li class="nav-list-item"><a href="#page-introduction">Introduction</a></li>
+					<li class="nav-list-item"><a href="#page-theproblem">The problem</a></li>
+					<li class="nav-list-item"><a href="#page-solution">The solution</a></li>
+					<li class="nav-list-item"><h1>De Glas Tornado 3000</h1></li>
+					<li class="nav-list-item"><a href="#page-introduction">The product</a></li>
+					<li class="nav-list-item"><a href="#page-introduction">Gallery</a></li>
 				</ul>
 			</nav>
 		</div>
 	</header>
-	<section class="page-introduction" id="page-introduction">
-		<div class="wrapper">
-			Introductie content
-		</div>
-	</section>
-	<section class="page-highlight" id="page-highlight">
-		<div class="wrapper">
-			Highlight tussenblok
-		</div>
-	</section>
-	<footer class="page-footer">
+	<main class="main">
+		<section class="mt75 mb100" id="page-theproblem">
+			<article class="information-block wrapper flex">
+				<div class="information-block-text">
+					<h2>The problem</h2>
+					<h4>Analyse</h4>
+					<p>
+						Maak een smartobject voor in de kroeg/sportlocatie of winkel die ervaringen kunnen verbeteren. Team 23 heeft ervoor gekozen om ervaringen in de kroeg van zowel de klant als de eigenaar te verbeteren.
+					</p>
+					<h4>Empathise</h4>
+					<p>
+						Een interview is afgenomen om team 23 een kijkje te geven door de ogen van de bartender. Deze staat hieronder uitgeschreven,
+						Uit het interview blijkt dat glazen vaak niet terug gebracht worden tot ergenis van de kroegeigenaren en bartenders.
+					</p>
+				</div>
+				<div class="information-block-image-container">
+					<div class="information-block-image" style="background-image: url('./public/img/IMG-20170920-WA0009.jpg');"></div>
+				</div>
+			</article>
+
+			<article class="information-block wrapper flex">
+				<div class="information-block-image-container">
+					<div class="information-block-image" style="background-image: url('./public/img/IMG-20170920-WA0003.jpg');"></div>
+				</div>
+				<div class="information-block-text">
+					<h2>The interview</h2>
+					<p>
+						<h4 class="mt20 mb10">Empathise</h4>
+						-- Plek voor interview als deze klaar is
+					</p>
+				</div>
+			</article>
+		</section>
+		<section class="page-highlight" id="page-highlight">
+			<div class="highlight-container wrapper flex">
+				<h2>
+					STOP! Tijd voor een pintje
+					<svg class="icon icon-beermug"><use xlink:href="./public/icons/icons.svg#icon-Beer-Mug"></use></svg>
+				</h2>
+			</div>
+		</section>
+		<section class="mt100 mb100" id="page-solution">
+			<article class="information-block wrapper flex">
+				<div class="information-block-text">
+					<h2>The problem</h2>
+					<h4>Analyse</h4>
+					<p>
+						Maak een smartobject voor in de kroeg/sportlocatie of winkel die ervaringen kunnen verbeteren. Team 23 heeft ervoor gekozen om ervaringen in de kroeg van zowel de klant als de eigenaar te verbeteren.
+					</p>
+					<h4>Empathise</h4>
+					<p>
+						Een interview is afgenomen om team 23 een kijkje te geven door de ogen van de bartender. Deze staat hieronder uitgeschreven,
+						Uit het interview blijkt dat glazen vaak niet terug gebracht worden tot ergenis van de kroegeigenaren en bartenders.
+					</p>
+				</div>
+				<div class="information-block-image-container" style="background-image: url('./public/img/background-wood.jpg');">
+					<div class="information-block-image" style="background-image: url('./public/img/IMG-20170920-WA0009.jpg');"></div>
+				</div>
+			</article>
+
+			<article class="information-block wrapper flex">
+				<div class="information-block-image-container" style="background-image: url('./public/img/background-wood.jpg');">
+					<div class="information-block-image" style="background-image: url('./public/img/IMG-20170920-WA0003.jpg');"></div>
+				</div>
+				<div class="information-block-text">
+					<h2>The interview</h2>
+					<p>
+						<h4 class="mt20 mb10">Empathise</h4>
+						-- Plek voor interview als deze klaar is
+					</p>
+				</div>
+			</article>
+		</section>
+	</main>
+	<footer class="page-footer" style="background-image: url('./public/img/background-wood.jpg');">
 		<div class="wrapper">
 			Footer
 		</div>
